9a32938feeb340f5d6d450132b4ca46a7b44d4e7
Prathameshh12 <prathamesh.ahire1204@gmail.com>
2025-06-29 22:22:40 +1000

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,6 +30,13 @@ const projects = [
     github: "https://github.com/Prathameshh12/SpacePortfolio",
     live: "https://prathameshspace.netlify.app"
   },
+   {
+    title: "iOS App - PetMate",
+    desc: "A playful SwiftUI to-do list app for pets that helps users track pet care routines, celebrate milestones, and stay connected through a fun Tinder-style task interaction.",
+    image: "/images/PetMate.png",
+    github: "https://github.com/Prathameshh12/PetMate",
+    live: "https://drive.google.com/file/d/1kzh6h3ZaEWj7tLbq0K10x04TVNWvoK9l/view"
+  },
 ];
 
 export default function Projects() {
